5c89e884e0def05ae81a0e6ef05b5ec601bd28b3
sglkc <31957516+sglkc@users.noreply.github.com>
2022-11-23 09:10:33 +0700

    fix `/src/rss.xml.ts`

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,8 +1,15 @@
 import rss from '@astrojs/rss';
 
+const postsImport = import.meta.glob('./blog/**/*.md', { eager: true });
+const posts = Object.values(postsImport);
+
 export const get = () => rss({
   title: 'Home // sglkc',
   description: '@sglkc Personal Website',
   site: import.meta.env.SITE,
-  items: import.meta.glob('./blog/**/*.md'),
+  items: posts.map((post: any) => ({
+    link: post.url,
+    title: post.frontmatter.title,
+    pubDate: post.frontmatter.created
+  })),
 });
