c0d9e978ce0e341208cadd8cadb20dd4d9311026
sglkc <31957516+sglkc@users.noreply.github.com>
2022-12-29 19:20:34 +0700

    feat: add Footer, Navbar, and ThemeButton component
    feat: add About page
    feat: add PageLayout layou

--- unocss.config.mjs
+++ unocss.config.mjs
@@ -9,6 +9,15 @@ export default defineConfig({
       cssExtend: {
         button: {
           color: 'var(--un-prose-links)'
+        },
+        hr: {
+          border: 'unset',
+          'border-top': 'solid 2px var(--un-prose-hr)',
+          transition: 'border-color 300ms cubic-bezier(0.4, 0, 0.2, 1)',
+          filter: 'brightness(0.925)'
+        },
+        '.dark hr': {
+          filter: 'brightness(1)'
         }
       }
     })
@@ -16,6 +25,9 @@ export default defineConfig({
   theme: {
     fontFamily: {
       overpass: '"Overpass Mono", monospace'
-    }
+    },
   },
+  shortcuts: {
+    'background': 'bg-zinc-100 dark:bg-zinc-800 transition-[background\-color] duration-300 ease-in-out',
+  }
 });
