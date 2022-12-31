d4bca944fcd6c99bac2443d48285ff3016f8076e
sglkc <31957516+sglkc@users.noreply.github.com>
2022-12-31 21:42:25 +0700

    build: remove @astrojs/image & sharp
    chore: ProjectPreview component props type
    chore: change Image to img element

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import path from 'path';
 import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import preact from "@astrojs/preact";
 import prefetch from '@astrojs/prefetch';
@@ -9,9 +8,6 @@ import Unocss from 'unocss/astro';
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp'
-    }),
     mdx(),
     preact(),
     prefetch(),
