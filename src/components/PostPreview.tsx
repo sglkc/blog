48b40d1c3c5f7ba0cd7bbb1f0a30976678ba11af
Seya <31957516+sglkc@users.noreply.github.com>
2024-04-08 05:26:17 +0700

    Revert "feat(SearchPost): convert to static component"
    This reverts commit fd76bc83248b314c1de618df63c4367992974047.

---
+++ src/components/PostPreview.tsx
@@ -0,0 +1,42 @@
+import type { CollectionEntry } from "astro:content";
+
+type Props = Omit<CollectionEntry<'posts'>['data'], 'tags'> & {
+  url: string
+  tags: string
+}
+
+export default function PostPreview(props: Props) {
+  const { url, title, description, created, thumbnail, tags } = props;
+
+  return (
+    <div class="my-6 py-2 flex gap-6 justify-between w-full">
+      <div class="flex flex-col">
+        <p class="mt-0 text-xs sm:text-sm">{created}</p>
+        <a class="decoration-none" href={url} target="_self">
+          <h1
+            class="my-0 text-lg sm:text-xl"
+            dangerouslySetInnerHTML={{ __html: title }}
+          />
+        </a>
+        <p
+          class="mt-2 text-sm sm:text-base"
+          dangerouslySetInnerHTML={{ __html: description }}
+        />
+        <div
+          class="my-0 flex flex-wrap gap-x-4 text-sm"
+          dangerouslySetInnerHTML={{ __html: tags }}
+        />
+      </div>
+      { thumbnail &&
+        <img
+          class="my-auto aspect-square object-cover w-[25%]"
+          src={'/assets/post/' + thumbnail}
+          alt={title + ' thumbnail image'}
+          draggable={false}
+          width="25%"
+          height="100%"
+        />
+      }
+    </div>
+  );
+}
